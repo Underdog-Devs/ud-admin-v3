f6640f0c8477c5aa7e9633648dc140cc0d9700df
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-19 15:42:13 -0500

    Updated image fetching to for use with supabase

--- components/blog/post.tsx
+++ components/blog/post.tsx
@@ -1,41 +1,72 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import Details from './details';
-import styles from './post.module.scss';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import Details from "./details";
+import styles from "./post.module.scss";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-function Post(props: any) {
-	const { post } = props;
-	function truncateString(postTitle: string) {
-		if (postTitle.length > 70) {
-			postTitle = `${postTitle.slice(0, 70)}...`;
-		}
-		return postTitle;
-	}
+function Post({ post }: { post: any }) {
+  const { id, title, image, first_paragraph, created_at } = post;
+  const supabase = createClientComponentClient();
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
-	return (
-		<div className={styles.container}>
-			<div>
-				{post.image ? (
-					<img
-						className={styles.img}
-						src={post.image}
-						alt="Featured"
-						loading="lazy"
-					/>
-				) : (
-					<Image src="/images/fallback.png" width="64" height="80" alt={''} />
-				)}
-			</div>
-			<div>
-			<h3><Link href={`/blog/${post.title.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9\s-]/g, '')}/${post.id}`}>
-					{truncateString(post.title)}
-				</Link></h3>
-				{post.first_paragraph}
-				<Details id={post.id} date={post.created_at} />
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    if (image) downloadImage(image);
+  }, [image, supabase]);
+
+  async function downloadImage(path: string) {
+    try {
+      const { data, error } = await supabase.storage
+        .from("images")
+        .download(path);
+      if (error) {
+        throw error;
+      }
+
+      const url = URL.createObjectURL(data);
+      setImageUrl(url);
+    } catch (error) {
+      console.log("Error fetching image in post.tsx!");
+    }
+  }
+
+  function truncateString(postTitle: string) {
+    if (postTitle.length > 70) {
+      postTitle = `${postTitle.slice(0, 70)}...`;
+    }
+    return postTitle;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div>
+        {imageUrl ? (
+          <img
+            className={styles.img}
+            src={imageUrl}
+            alt="Featured"
+            loading="lazy"
+          />
+        ) : (
+          <Image src="/images/fallback.png" width="64" height="80" alt={""} />
+        )}
+      </div>
+      <div>
+        <h3>
+          <Link
+            href={`/blog/${title
+              .replace(/\s+/g, "-")
+              .replace(/[^a-zA-Z0-9\s-]/g, "")}/${id}`}
+          >
+            {truncateString(title)}
+          </Link>
+        </h3>
+        {first_paragraph}
+        <Details id={id} date={created_at} />
+      </div>
+    </div>
+  );
 }
 
 export default Post;
