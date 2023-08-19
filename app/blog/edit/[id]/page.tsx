048bd36e9972f66b81fff19e61b8405200cc27c2
Al <al@Als-MacBook-Pro.local>
2023-08-18 23:21:21 -0700

    Added form validation messages

--- app/blog/edit/[id]/page.tsx
+++ app/blog/edit/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import StarterKit from "@tiptap/starter-kit";
 import Highlight from "@tiptap/extension-highlight";
@@ -13,13 +13,15 @@ import Nav from "@/components/dashboard/nav";
 import { Input } from "@/components/input";
 import { redirect } from "next/navigation";
 import { useParams } from "next/navigation";
+import { PostBlogSchema } from "@/lib/schema";
 
 function EditPost() {
   const { id } = useParams();
   const [firstParagraph, setFirstParagraph] = useState<string>("Loading...");
-  const [fileSizeWarning, setFileSizeWarning] = useState("");
   const [postTitle, setPostTitle] = useState("Loading...");
   const [didFetch, setDidFetch] = useState<boolean>(false);
+  const [validationError, setValidationError] = useState<any>(null);
+  const formRef = useRef<HTMLFormElement>(null);
 
   const supabase = createClientComponentClient();
 
@@ -55,52 +57,17 @@ function EditPost() {
     }
   }
 
-  let file: File | null = null;
-
-  function handleFiles(
-    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) {
-    const files = (e.target as HTMLInputElement).files;
-    file = files![0];
-    // Check file size and set warning if it's too large
-    if (file.size > 1572864) {
-      setFileSizeWarning(
-        "File size exceeds 1.5MB. Please choose a smaller file."
-      );
-      file = null;
-    } else {
-      setFileSizeWarning("");
-    }
-  }
-
-  async function handleImageUpload() {
-    const datePrefix = Date.now();
-    const fileName = `media/${datePrefix}-${file!.name}`;
-    // Get signed key for file name in current directory.
-    const response = await fetch(`/api/presignedurl?fileName=${fileName}`);
-
-    const data = await response.json();
-    const url = data.signedUrlObject.url;
-    fetch(url, {
-      method: "PUT",
-      body: file,
-    })
-      .then((response) => {
-        if (!response.ok) {
-          return `Error uploading ${fileName}.`;
-        }
-      })
-      .catch((err) => {
-        return `Error uploading ${fileName}.`;
-      });
-    return `${process.env.NEXT_PUBLIC_S3_URL}${fileName}`;
-  }
 
   async function postBlog() {
-    let image = "";
-    if (file) {
-      image = await handleImageUpload();
+    const result = PostBlogSchema.safeParse({
+      title: postTitle, entry: tipTapEditor?.getJSON()?.content?.[0]?.content?.[0]?.text
+    })
+
+    if (result.success === false) {
+      return { error: result.error.format() }
     }
+
+    let image = "";
 
     const {
       data: { user },
@@ -127,12 +94,26 @@ function EditPost() {
     setPostTitle(e.target.value);
   }
 
+
+  async function action() {
+    const result = await postBlog()
+    if (result?.error) {
+      setValidationError(result?.error)
+    } else {
+      if (formRef.current) {
+        formRef.current.reset()
+        setValidationError(null)
+      }
+    }
+  }
+
+
   return (
     <div className={styles.container}>
       <div>
         <Nav />
       </div>
-      <form className={styles.main} action={postBlog}>
+      <form className={styles.main} action={action}>
         <div className={styles.topContainer}>
           <div>
             <Input labelFor="title" labelText="Title">
@@ -144,23 +125,43 @@ function EditPost() {
                 value={postTitle || ""}
               />
             </Input>
+            {validationError?.title && (
+              <div className={styles.inputWarning}>
+                {
+                  validationError?.title &&
+                  validationError.title._errors.map((error: string, index: number) => (
+                    <p key={index} className={styles.inputWarning}>
+                      {error}
+                    </p>
+                  ))
+                }
+              </div>
+            )}
             <Input labelFor="featured-image" labelText="Featured Image">
               <input
-                onChange={handleFiles}
                 id="featured-image"
                 type="file"
                 accept="image/*"
               />
             </Input>
-            {fileSizeWarning && (
-              <p style={{ color: "red" }}>{fileSizeWarning}</p>
-            )}
           </div>
         </div>
 
         <TipTapEdit editor={tipTapEditor} />
 
         <EditorContent editor={tipTapEditor} content={"Test"} />
+        {validationError?.entry && (
+          <div className={styles.inputWarning}>
+            {
+              validationError?.entry &&
+              validationError.entry._errors.map((error: string, index: number) => (
+                <p key={index} className={styles.inputWarning}>
+                  {error}
+                </p>
+              ))
+            }
+          </div>
+        )}
 
         <div className={styles.buttons}>
           <button type="submit">Publish</button>
