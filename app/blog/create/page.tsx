28468b64664eea5f3d77059ee2a122ecba4adbc8
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-20 20:42:33 -0500

    Create post overhual
    Added:
    - image uploading using Supabase Storage
    - error handling on inserting new post in DB
    - refactored const functions into named functions
    - disable buttons during post uploading
    - client side error messages on new post submit failure

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,163 +1,195 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useRootContext } from '../../context/RootContext'
-import StarterKit from '@tiptap/starter-kit';
-import Highlight from '@tiptap/extension-highlight';
-import Typography from '@tiptap/extension-typography';
-import { Image as tiptapImage } from '@tiptap/extension-image';
-import TextAlign from '@tiptap/extension-text-align';
-import { useEditor } from '@tiptap/react';
-import TipTapEdit from '@/components/blog/tiptapEditor/tiptap-edit';
-import styles from './create.module.scss';
-import Nav from '@/components/dashboard/nav';
-import { Input } from '@/components/input';
+import React, { useEffect, useState } from "react";
+import {
+  User,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import StarterKit from "@tiptap/starter-kit";
+import Highlight from "@tiptap/extension-highlight";
+import Typography from "@tiptap/extension-typography";
+import { Image as tiptapImage } from "@tiptap/extension-image";
+import TextAlign from "@tiptap/extension-text-align";
+import { JSONContent, useEditor } from "@tiptap/react";
+import TipTapEdit from "@/components/blog/tiptapEditor/tiptap-edit";
+import styles from "./create.module.scss";
+import Nav from "@/components/dashboard/nav";
+import { Input } from "@/components/input";
 import { redirect } from "next/navigation";
 
 function CreatePost() {
-	const [firstParagraph, setFirstParagraph] = useState<string>();
-	const [fileSizeWarning, setFileSizeWarning] = useState('');
+  const [entry, setEntry] = useState<JSONContent | null>(null);
+  const [firstParagraph, setFirstParagraph] = useState<string | null>(null);
+  const [postTitle, setPostTitle] = useState<string | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [file, setFile] = useState<File | null>(null);
+  const [fileSizeWarning, setFileSizeWarning] = useState<string | null>(null);
+  const [postCreationError, setPostCreationError] = useState<Error | null>(
+    null
+  );
+  const [submitting, setSubmitting] = useState<boolean>(false);
 
-	let file: File | null = null;
-	const supabase = createClientComponentClient()
+  const supabase = createClientComponentClient();
 
-	const { blogTitle, setBlogTitle } = useRootContext();
-	useEffect(() => {
-		setBlogTitle('');
-	}, []);
+  useEffect(() => {
+    getUser();
+  }, [supabase]);
 
-	function handleFiles(
-		e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-	) {
-		const files = (e.target as HTMLInputElement).files;
-		file = files![0];
-		// Check file size and set warning if it's too large
-		if (file.size > 1572864) {
-			setFileSizeWarning('File size exceeds 1.5MB. Please choose a smaller file.');
-			file = null;
-		} else {
-			setFileSizeWarning('');
-		}
-	}
+  async function getUser() {
+    try {
+      const { data, error } = await supabase.auth.getUser();
 
-	const tipTapEditor = useEditor({
-		extensions: [
-			StarterKit,
-			Highlight,
-			Typography,
-			tiptapImage,
-			TextAlign.configure({
-				types: ['heading', 'paragraph'],
-			}),
-		],
-		onUpdate({ editor }) {
-			if (!editor.getJSON().content![0].content) return;
-			const paragraph = editor.getJSON().content![0].content![0].text;
-			if (paragraph) {
-				setFirstParagraph(
-					paragraph.length < 120
-						? paragraph.substring(0, 120)
-						: `${paragraph.substring(0, 120)}...`,
-				);
-			}
-		},
-	});
+      if (error) {
+        throw error;
+      }
 
+      setUser(data.user);
+    } catch (error) {
+      console.log("There was an error fetching User.\n", "Error: ", error);
+    }
+  }
 
-	async function handleImageUpload() {
-		const datePrefix = Date.now();
-		const fileName = `media/${datePrefix}-${file!.name}`;
-		// Get signed key for file name in current directory.
-		const response = await fetch(
-			`/api/presignedurl?fileName=${fileName}`
-		);
+  function handleFiles(event: React.ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files || event.target.files.length === 0) {
+      setFileSizeWarning(null);
+      setFile(null);
+      return;
+    }
+    const file = event.target.files[0];
+    // Check file size and set warning if it's too large
+    if (file && file.size > 1572864) {
+      setFileSizeWarning(
+        "File size exceeds 1.5MB. Please choose a smaller file."
+      );
+      setFile(null);
+    } else {
+      setFileSizeWarning(null);
+      setFile(file);
+    }
+  }
 
-		const data = await response.json();
-		const url = data.signedUrlObject.url;
-		fetch(url, {
-			method: "PUT",
-			body: file,
-		})
-			.then((response) => {
-				if (!response.ok) {
-					return `Error uploading ${fileName}.`;
-				}
-			})
-			.catch((err) => {
-				return `Error uploading ${fileName}.`
-			});
-		return `${process.env.NEXT_PUBLIC_S3_URL}${fileName}`
-	}
+  const tipTapEditor = useEditor({
+    extensions: [
+      StarterKit,
+      Highlight,
+      Typography,
+      tiptapImage,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+    onUpdate() {
+      if (tipTapEditor) {
+        setEntry(tipTapEditor.getJSON());
+        setFirstParagraph(tipTapEditor.getText());
+      }
+    },
+  });
 
-	const postBlog = async () => {
-		let image = '';
+  // image filepath, user id, editor and editor entry, first paragraph, post title
+  async function postBlog() {
+    try {
+      let imagePath: string | null = null;
+      // check if user is null, throws error
+      if (!user?.id) throw new Error("Unable to find user id or id is invalid");
+      // check for editor, throws error
+      if (!tipTapEditor) throw new Error("Editor failed to load");
+      // attempt to upload image and get url, throws error
+      if (file) {
+        const fileExt = file.name.split(".").pop();
+        imagePath = `${user.id}-${Math.random()}.${fileExt}`;
 
-		if(file){
-			image = await handleImageUpload();
-		}
+        const { error: uploadError } = await supabase.storage
+          .from("images")
+          .upload(imagePath, file);
 
-		const {
-			data: { user },
-		} = await supabase.auth.getUser();
+        if (uploadError) {
+          throw uploadError;
+        }
+      }
+      // attempt to add post to DB, throws error
+      const { error: postInsertError } = await supabase.from("posts").insert({
+        entry,
+        author: user?.id,
+        title: postTitle,
+        first_paragraph: firstParagraph,
+        image: imagePath, // will get this after uploading image
+      });
 
-		const { data, error } = await  supabase.from('posts').insert({ 
-			entry: tipTapEditor?.getJSON(),
-			author: user?.id,
-			title: blogTitle,
-			first_paragraph: firstParagraph,
-			image
-		})
-		if (!error) {
-			redirect('/blog');
-		}
-	};
+      if (postInsertError) {
+        throw postInsertError;
+      }
+      redirect("/blog");
+    } catch (error: any) {
+      setPostCreationError(error);
+      setSubmitting(false);
+    }
+  }
 
-	const eraseBlog = () => {
-		tipTapEditor?.commands.clearContent();
-	};
+  function eraseBlog() {
+    tipTapEditor?.commands.clearContent();
+    setEntry(null);
+    setFirstParagraph(null);
+  }
 
-	const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setBlogTitle(e.target.value);
-	};
+  function onTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setPostTitle(e.target.value);
+  }
 
-
-	return (
-		<div className={styles.container}>
-			<div>
-				<Nav />
-			</div>
-			<form className={styles.main} action={postBlog}>
-				<div className={styles.topContainer}>
-					<div>
-						<Input labelFor="title" labelText="Title" required>
-							<input
-								id="title"
-								type="text"
-								className={styles.titleInput}
-								onChange={onTitleChange}
-								value={blogTitle || ''}
-								required
-							/>
-						</Input>
-						<Input labelFor="featured-image" labelText="Featured Image">
-							<input
-								onChange={handleFiles}
-								id="featured-image"
-								type="file"
-								accept="image/*"
-							/>
-						</Input>
-						{fileSizeWarning && <p style={{ color: 'red' }}>{fileSizeWarning}</p>}
-					</div>
-				</div>
-				<TipTapEdit editor={tipTapEditor} />
-				<div className={styles.buttons}>
-					<button type='submit'>Publish</button>
-					<button className={styles.clearButton} onClick={eraseBlog}>Clear</button>
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <div>
+        <Nav />
+      </div>
+      <form className={styles.main} action={postBlog}>
+        <div className={styles.topContainer}>
+          <div>
+            <Input labelFor="title" labelText="Title" required>
+              <input
+                id="title"
+                type="text"
+                className={styles.titleInput}
+                onChange={onTitleChange}
+                value={postTitle || ""}
+                required
+              />
+            </Input>
+            <Input labelFor="featured-image" labelText="Featured Image">
+              <input
+                onChange={handleFiles}
+                id="featured-image"
+                type="file"
+                accept="image/*"
+              />
+            </Input>
+            {fileSizeWarning && (
+              <p style={{ color: "red" }}>{fileSizeWarning}</p>
+            )}
+          </div>
+        </div>
+        <TipTapEdit editor={tipTapEditor} />
+        {(!firstParagraph || firstParagraph.length < 120) && (
+          <p style={{ color: "red" }}>
+            Post must contain at least 120 characters.
+          </p>
+        )}
+        <div className={styles.buttons}>
+          <button type="submit" disabled={submitting}>
+            Publish
+          </button>
+          <button
+            className={styles.clearButton}
+            onClick={eraseBlog}
+            disabled={submitting}
+          >
+            Clear
+          </button>
+        </div>
+        {postCreationError && (
+          <p style={{ color: "red" }}>{postCreationError.message}</p>
+        )}
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;
