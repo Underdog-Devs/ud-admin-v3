05984f92b29cc2282df897d96b403c3c8ae8ddd6
Kenson Johnson <kenson.johnson@outlook.com>
2023-08-20 21:24:45 -0500

    Generalized to just handling image fetching

--- components/blog/PostImage.tsx
+++ components/blog/PostImage.tsx
@@ -4,20 +4,9 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
 import styles from "./PostImage.module.scss";
 
-export function PostImage({
-  uid,
-  url,
-  size,
-  onUpload,
-}: {
-  uid: string;
-  url: string | null;
-  size: number;
-  onUpload: (url: string) => void;
-}) {
+export function PostImage({ url, size }: { url: string | null; size: number }) {
   const supabase = createClientComponentClient();
-  const [imageUrl, setImageUrl] = useState<string | null>(url);
-  const [uploading, setUploading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
   useEffect(() => {
     async function downloadImage(path: string) {
@@ -39,70 +28,44 @@ export function PostImage({
     if (url) downloadImage(url);
   }, [url, supabase]);
 
-  async function uploadImage(event: React.ChangeEvent<HTMLInputElement>) {
-    try {
-      setUploading(true);
+  // async function uploadImage(event: React.ChangeEvent<HTMLInputElement>) {
+  //   try {
+  //     setUploading(true);
 
-      if (!event.target.files || event.target.files.length === 0) {
-        throw new Error("You must select an image to upload.");
-      }
+  //     if (!event.target.files || event.target.files.length === 0) {
+  //       throw new Error("You must select an image to upload.");
+  //     }
 
-      const file = event.target.files[0];
-      const fileExt = file.name.split(".").pop();
-      const filePath = `${uid}-${Math.random()}.${fileExt}`;
+  //     const file = event.target.files[0];
+  //     const fileExt = file.name.split(".").pop();
+  //     const filePath = `${uid}-${Math.random()}.${fileExt}`;
 
-      let { error: uploadError } = await supabase.storage
-        .from("images")
-        .upload(filePath, file);
+  //     let { error: uploadError } = await supabase.storage
+  //       .from("images")
+  //       .upload(filePath, file);
 
-      if (uploadError) {
-        throw uploadError;
-      }
+  //     if (uploadError) {
+  //       throw uploadError;
+  //     }
 
-      onUpload(filePath);
-    } catch (error) {
-      alert("Error uploading image!");
-    } finally {
-      setUploading(false);
-    }
-  }
+  //     onUpload(filePath);
+  //   } catch (error) {
+  //     alert("Error uploading image!");
+  //   } finally {
+  //     setUploading(false);
+  //   }
+  // }
 
   return (
     <div className={styles.container}>
-      {imageUrl ? (
-        <Image
-          width={size}
-          height={size}
-          src={imageUrl}
-          alt="Post Image"
-          className={`${styles.post} ${styles.image}`}
-          style={{ height: size, width: size }}
-        />
-      ) : (
-        <div
-          className={`${styles.post} ${styles["no-image"]}`}
-          style={{ height: size, width: size }}
-        />
-      )}
-      <div style={{ width: size }}>
-        <label
-          className={`${styles.button} ${styles.primary} ${styles.block}`}
-          htmlFor="single"
-        >
-          {uploading ? "Uploading ..." : "Upload"}
-        </label>
-        <input
-          style={{
-            visibility: "hidden",
-            position: "absolute",
-          }}
-          type="file"
-          id="single"
-          accept="image/*"
-          onChange={uploadImage}
-          disabled={uploading}
-        />
-      </div>
+      <Image
+        width={size}
+        height={size}
+        src={imageUrl ?? "/images/fallback.png"}
+        alt="Post Image"
+        className={`${styles.post} ${styles.image}`}
+        style={{ height: size, width: size, objectFit: "contain" }}
+      />
     </div>
   );
 }
