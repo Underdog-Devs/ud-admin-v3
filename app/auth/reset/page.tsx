1cf50ee125dfa4a1eadcc3c71784aa3c6fe43176
Al <al@Als-MacBook-Pro.local>
2023-08-29 16:48:55 -0700

    Login component refactoring and password reset validation

--- app/auth/reset/page.tsx
+++ app/auth/reset/page.tsx
@@ -1,23 +1,41 @@
 'use client'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import styles from './reset.module.scss'
+import { AuthenticationSchema } from '@/lib/schema'
 
 export default function Reset() {
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const router = useRouter()
     const supabase = createClientComponentClient()
+    const [validationError, setValidationError] = useState<any>(null)
+    const formRef = useRef<HTMLFormElement>(null)
 
     const resetPassword = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+
+        const result = AuthenticationSchema.passwordReset.safeParse({ password, confirmPassword })
+
+        if (result.success) {
+            if (formRef.current) {
+                formRef.current.reset();
+                setValidationError(null);
+            }
+        } else {
+            setValidationError(result.error.format());
+            // throw new Error("Validation failed");
+        }
+
         if (password === confirmPassword) {
             const { data, error } = await supabase.auth.updateUser({ password })
             await supabase.auth.refreshSession();
-            router.push('/')
+            router.push('/dashboard')
         }
     }
+
+
 
     return (
         <div
@@ -55,10 +73,15 @@ export default function Reset() {
                         />
                     </div>
 
+                    {validationError?.confirmPassword && (
+                        <p style={{width:150, color: 'white'}}>
+                            {validationError.confirmPassword._errors.join(', ')}
+                        </p>
+                    )}
                     <div className={styles.formActions}>
-                    <button type="submit" className={styles.formButton}>
-                        Reset
-                    </button>
+                        <button type="submit" className={styles.formButton}>
+                            Reset
+                        </button>
                     </div>
                 </form>
             </div>
